639e373275966fa595e94714c49e1624a0f15474
kitty244 <33192304+kitty244@users.noreply.github.com>
2022-02-10 13:21:37 +0100

    DAY 2 Assignment Submission

---
+++ DAY2.js
@@ -0,0 +1,43 @@
+
+// Arithmetic Operator
+
+var number1 = 200;
+var number2 = 200;
+
+var answer = number1 + number2;
+console.log(answer);
+
+// Assignment Operator
+
+var number3 = 10;
+ number3 += 5;
+
+console.log(number3);
+
+// Comparison Operator
+
+var number4 = 400;
+var number5 = 200;
+
+console.log(number4 <= number5);
+
+// Identity Operator
+
+var number6 = 200;
+var number7 = 200;
+
+console.log(number6 === number7);
+
+// Conditional Operator
+
+var number8 = 200;
+var number9 = 200;
+
+console.log(number8 || number9);
+
+// Unary Operator 
+
+var number11 = 10;
+var number12 = number11--;
+
+console.log(number11 , number12);
